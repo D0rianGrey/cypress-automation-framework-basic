974f1ed3b20245eff73e92ba3ff07f8e03f7481c
D0rianGrey <utyfdfrthby08@gmail.com>
2025-03-12 18:58:05 +0100

    Update Cypress configuration and reports setup in GitHub Actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,23 +1,16 @@
-const { defineConfig } = require('cypress');
+const {defineConfig} = require('cypress');
 
 module.exports = defineConfig({
-  projectId: 'f8es42',
-  e2e: {
-    baseUrl: 'https://www.saucedemo.com',
-    specPattern: 'cypress/e2e/**/*.cy.js',
-    viewportWidth: 1280,
-    viewportHeight: 720,
-    video: false,
-    screenshotOnRunFailure: true,
-    reporter: 'mochawesome',
-    reporterOptions: {
-      reportDir: 'cypress/reports',
-      overwrite: false,
-      html: false,
-      json: true,
+    e2e: {
+        baseUrl: 'https://www.saucedemo.com',
+        specPattern: 'cypress/e2e/**/*.cy.js',
+        reporter: 'mochawesome',
+        reporterOptions: {
+            reportDir: 'cypress/reports/mocha',
+            quite: true,
+            overwrite: false,
+            html: false,
+            json: true
+        },
     },
-    setupNodeEvents(on, config) {
-      // реализация событий узла
-    },
-  },
 });
